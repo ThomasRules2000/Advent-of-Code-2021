32237568988407dc1bf9b811866e2a7490058846
Thomas Stacey <thomas.stacey@warwick.ac.uk>
2021-12-01 09:43:05 +0000

    Day 1

--- src/Days/Day01.hs
+++ src/Days/Day01.hs
@@ -10,10 +10,22 @@ type Output1 = Int
 type Output2 = Int
 
 parser :: String -> Input
-parser = undefined
+parser = fmap read . lines
 
 part1 :: Input -> Output1
-part1 nums = undefined
+part1 = numIncrease
+
+-- Explicit Recursion
+-- numIncrease :: [Int] -> Int
+-- numIncrease [] = 0
+-- numIncrease [x] = 0
+-- numIncrease (x:y:xs)
+--     | x < y = 1 + rest
+--     | otherwise = rest
+--     where rest = numIncrease (y:xs)
+
+numIncrease :: [Int] -> Int
+numIncrease xs = length $ filter id $ zipWith (<) xs $ tail xs
 
 part2 :: Input -> Output2
-part2 nums = undefined
+part2 nums = numIncrease $ zipWith (+) nums $ zipWith (+) (tail nums) $ drop 2 nums
