2138a81f938885fe52ca5b4e32d210d73b1df7a3
Thomas Stacey <thomas.stacey@warwick.ac.uk>
2021-12-05 13:38:25 +0000

    Day 5

--- src/Days/Day05.hs
+++ src/Days/Day05.hs
@@ -1,19 +1,47 @@
 module Days.Day05 where
-import qualified Program.RunDay as R (runDay)
+import           Control.DeepSeq (NFData)
+import           Data.List.Split
+import           Data.Map        (Map)
+import qualified Data.Map.Strict as Map
+import           Data.Set        (Set)
+import qualified Data.Set        as Set
+import           GHC.Generics    (Generic)
+import qualified Program.RunDay  as R (runDay)
+import           Util.Util
 
 runDay :: String -> IO (Maybe Integer, Maybe Integer)
 runDay = R.runDay parser part1 part2
 
-type Input = [Int]
+type Input = [Line]
 
 type Output1 = Int
 type Output2 = Int
 
+type Point = (Int, Int)
+
+data Line = Line Point Point
+    deriving (Eq, Ord, Show, Generic, NFData)
+
 parser :: String -> Input
-parser = undefined
+parser = map (uncurry Line . listToTuple . map (listToTuple . map read . splitOn ",") . splitOn " -> ") . lines
 
 part1 :: Input -> Output1
-part1 nums = undefined
+part1 = overlaps
+      . filter (\(Line (x1, y1) (x2, y2)) -> x1 == x2 || y1 == y2)
+
+overlaps :: [Line] -> Int
+overlaps = Map.size
+         . Map.filter (>1) 
+         . Map.unionsWith (+) 
+         . map (Map.fromSet (const 1) . points) 
+
+points :: Line -> Set Point
+points (Line p1@(x1, y1) p2@(x2, y2))
+    | p1 == p2 = Set.singleton p1
+    | otherwise = Set.fromList $ zip xs ys
+    where
+        xs = [x1, x1 + signum (x2 - x1) .. x2]
+        ys = [y1, y1 + signum (y2 - y1) .. y2]
 
 part2 :: Input -> Output2
-part2 nums = undefined
+part2 = overlaps
