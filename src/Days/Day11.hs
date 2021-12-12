6166ae54bb03c32d73ad9665a615e32870d43d1a
Thomas Stacey <thomas.stacey@warwick.ac.uk>
2021-12-12 15:49:49 +0000

    Day 11

--- src/Days/Day11.hs
+++ src/Days/Day11.hs
@@ -1,19 +1,45 @@
 module Days.Day11 where
-import qualified Program.RunDay as R (runDay)
+import           Data.Bifunctor  (first)
+import           Data.Char       (digitToInt)
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import           Data.Set        (Set)
+import qualified Data.Set        as Set
+import qualified Program.RunDay  as R (runDay)
+import           Util.Util       (gridToMap)
 
 runDay :: String -> IO (Maybe Integer, Maybe Integer)
 runDay = R.runDay parser part1 part2
 
-type Input = [Int]
+type Pos = (Int, Int)
+
+type Input = Map Pos Int
 
 type Output1 = Int
 type Output2 = Int
 
 parser :: String -> Input
-parser = undefined
+parser = gridToMap . map (map digitToInt) . lines
 
 part1 :: Input -> Output1
-part1 nums = undefined
+part1 = fst . (!!100) . iterate (\(n, m) -> first (+n) $ doCycle m) . (0,)
+
+doCycle :: Map Pos Int -> (Int, Map Pos Int)
+doCycle = getFlashes Set.empty . fmap (+1)
+
+getFlashes :: Set Pos -> Map Pos Int -> (Int, Map Pos Int)
+getFlashes flashes octos
+    | null newFlashes = (Set.size flashes, Map.union (Map.fromSet (const 0) flashes) octos)
+    | otherwise = getFlashes (Set.union flashes newFlashes) $ foldl flash octos newFlashes
+    where
+        newFlashes = Map.keysSet (Map.filter (>9) octos) Set.\\ flashes
+        flash m = foldr (Map.adjust (+1)) m . getAround
+
+getAround :: Pos -> [Pos]
+getAround (x0, y0) = [(x+x0, y+y0) | x <- [-1..1], y <- [-1..1], x/=0 || y/=0]
 
 part2 :: Input -> Output2
-part2 nums = undefined
+part2 m
+    | flashed == Map.size m = 1
+    | otherwise = 1 + part2 newMap
+    where (flashed, newMap) = doCycle m
--- src/Util/Util.hs
+++ src/Util/Util.hs
@@ -1,4 +1,6 @@
 module Util.Util where
+import           Data.Bifunctor  (first)
+import           Data.Foldable   (foldl')
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 
@@ -18,9 +20,21 @@ rotate :: Int -> [a] -> [a]
 rotate _ [] = []
 rotate n xs = zipWith const (drop n (cycle xs)) xs
 
+-- binToDec :: [Bool] -> Int
+-- binToDec = go . reverse
+--     where
+--         go :: [Bool] -> Int
+--         go []     = 0
+--         go (x:xs) = fromEnum x + 2 * go xs
+
+-- binToDec :: [Bool] -> Int
+-- binToDec = sum . map fst . filter snd . zip [2^x | x <-[0..]] . reverse
+
 binToDec :: [Bool] -> Int
-binToDec = go . reverse
-    where
-        go :: [Bool] -> Int
-        go [] = 0
-        go (x:xs) = fromEnum x + 2 * go xs
+binToDec = foldl' (\acc b -> 2*acc + fromEnum b) 0
+
+gridToMap :: [[a]] -> Map (Int, Int) a
+gridToMap = Map.fromList
+       . concat
+       . zipWith (\x -> map $ first (x,)) [0..]
+       . map (zip [0..])
