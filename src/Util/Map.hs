f28eedc47718a0fc5ca9bd7087dd81c6fe6e706e
Thomas Stacey <thomas.stacey@warwick.ac.uk>
2021-12-24 21:25:20 +0000

    Add Util.Map.invert

--- src/Util/Map.hs
+++ src/Util/Map.hs
@@ -1,7 +1,10 @@
 module Util.Map where
-import           Data.Bifunctor  (first)
+import           Data.Bifunctor  (first, second)
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
+import           Data.Set        (Set)
+import qualified Data.Set        as Set
+import           Data.Tuple      (swap)
 
 -- | Infix `union`
 (\/) :: Ord k => Map k v -> Map k v -> Map k v
@@ -21,3 +24,9 @@ fromGrid = Map.fromAscList
          . concat
          . zipWith (\x -> map $ first (x,)) [0..]
          . map (zip [0..])
+
+-- | Invert a map to give a map from values to sets of keys
+invert :: (Ord k, Ord v) => Map k v -> Map v (Set k)
+invert = Map.fromListWith Set.union 
+       . map (second Set.singleton . swap) 
+       . Map.toList
