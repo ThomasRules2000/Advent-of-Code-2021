f5d830e69d308db68eb7d4806ff91a2152676495
Thomas Stacey <thomas.stacey@warwick.ac.uk>
2021-12-26 01:50:42 +0000

    Fix Day 23 and add heuristic

--- src/Days/Day23.hs
+++ src/Days/Day23.hs
@@ -1,10 +1,8 @@
 module Days.Day23 where
-import           Data.Bifunctor
+import           Data.Bifunctor   (second)
 import           Data.Heap        (MinPrioHeap)
 import qualified Data.Heap        as Heap
-import           Data.List
-import           Data.Map.Strict  (Map)
-import qualified Data.Map.Strict  as Map
+import           Data.List        (find, intersperse, transpose)
 import           Data.Maybe       (isNothing)
 import           Data.Set         (Set)
 import qualified Data.Set         as Set
@@ -13,13 +11,14 @@ import           Data.Vector      (Vector)
 import qualified Data.Vector      as Vector
 import qualified Program.RunDay   as R (runDay)
 import           System.Clock     (TimeSpec)
-import           Util.Util
+import           Util.Util        (imap, listToTuple4, setAt, uncurry4)
 
 runDay :: String -> IO (Maybe TimeSpec, Maybe TimeSpec, Maybe TimeSpec)
 runDay = R.runDay parser part1 part2
 
 data Grid = Grid {
-    corridor :: Vector (Maybe Amphipod),
+    roomSize :: Int,
+    corridor :: [Maybe Amphipod],
     roomA    :: [Amphipod], -- Below space 2
     roomB    :: [Amphipod], -- Below space 4
     roomC    :: [Amphipod], -- Below space 6
@@ -29,12 +28,14 @@ data Grid = Grid {
 instance Show Grid where
     show Grid{..} = unlines $ ["#############", "#" ++ corridorString ++ "#"] ++ roomStrings ++ ["  #########"]
         where
-            corridorString = concatMap (maybe "." show) $ Vector.toList corridor
-            roomStrings = transpose $ ["# ", "# ", "##"] ++ intersperse "##" (map showRoom [roomA, roomB, roomC, roomD]) ++ ["##", "#", "#"]
+            corridorString = concatMap (maybe "." show) corridor
+            roomStrings = transpose $ [hashSpace, hashSpace, hashes] ++ intersperse hashes (map showRoom [roomA, roomB, roomC, roomD]) ++ [hashes, "#", "#"]
+
+            hashSpace = '#' : replicate (roomSize - 1) ' '
+            hashes = replicate roomSize '#'
 
             showRoom :: [Amphipod] -> String
-            showRoom [x] = '.':show x
-            showRoom xs  = concatMap show xs
+            showRoom xs  = replicate (roomSize - length xs) '.' ++ concatMap show xs
 
 data Amphipod = A | B | C | D
     deriving (Eq, Ord, Show)
@@ -44,8 +45,19 @@ type Input = Grid
 type Output1 = Int
 type Output2 = Int
 
+data AStarItem = AStarItem {
+    gValue :: Int,
+    hValue :: Int
+} deriving (Eq, Show)
+
+instance Ord AStarItem where
+    (AStarItem g1 h1) <= (AStarItem g2 h2) = g1 + h1 <= g2 + h2
+
+fValue :: AStarItem -> Int
+fValue AStarItem{..} = gValue + hValue
+
 parser :: String -> Input
-parser = uncurry4 (Grid $ Vector.replicate 11 Nothing)
+parser = uncurry4 (Grid 2 $ replicate 11 Nothing)
        . listToTuple4
        . map (map getAmphipod)
        . filter ((/= '#'). head)
@@ -62,7 +74,7 @@ getAmphipod 'D' = D
 getAmphipod  _  = error "Not an amphipod"
 
 part1 :: Input -> Output1
-part1 = search 2 (getTarget 2) Set.empty . Heap.singleton . (0,)
+part1 g = search Set.empty $ Heap.singleton (AStarItem 0 $ getHeuristic g, g)
 
 amphipodMoveCost :: Amphipod -> Int
 amphipodMoveCost A = 1
@@ -73,34 +85,65 @@ amphipodMoveCost D = 1000
 invalidSpaces :: Set Int
 invalidSpaces = Set.fromList [2,4,6,8]
 
-search :: Int -> Grid -> Set Grid -> MinPrioHeap Int Grid -> Int
-search roomSize target closedSet heap
-    | grid == target = cost
-    | Set.member grid closedSet = search roomSize target closedSet restHeap
-    | otherwise = search roomSize target (Set.insert grid closedSet) $ Heap.union restHeap $ step roomSize grid cost
+search :: Set Grid -> MinPrioHeap AStarItem Grid -> Int
+search closedSet heap
+    | isTarget grid = gValue cost
+    | Set.member grid closedSet = search closedSet restHeap
+    | otherwise = search (Set.insert grid closedSet) $ Heap.union restHeap $ step grid (gValue cost)
     where Just ((cost, grid), restHeap) = Heap.view heap
 
 sameAwayStates :: Grid -> Grid -> Bool
 sameAwayStates grid1 grid2 = all (uncurry (==) . (`both` (grid1, grid2))) [roomA, roomB, roomC, roomD]
 
-getTarget :: Int -> Grid
-getTarget n = Grid{corridor=Vector.replicate 11 Nothing, roomA=replicate n A, roomB=replicate n B, roomC=replicate n C, roomD=replicate n D}
+isTarget :: Grid -> Bool
+isTarget Grid{..} = all (==A) roomA && all (==B) roomB && all (==C) roomC && all (==D) roomD && all isNothing corridor
 
-step :: Int -> Grid -> Int -> MinPrioHeap Int Grid
-step roomSize grid@Grid{..} cost = Heap.union roomMoves corridorMoves
+step :: Grid -> Int -> MinPrioHeap AStarItem Grid
+step grid@Grid{..} cost = Heap.union roomMoves corridorMoves
     where
-        roomMoves = Heap.unions $ map (getRoomMoves roomSize grid cost) [A, B, C, D]
-        corridorMoves = getCorridorMoves roomSize grid cost
+        roomMoves = Heap.unions $ map (getRoomMoves grid cost) [A, B, C, D]
+        corridorMoves = getCorridorMoves grid cost
 
-getCorridorMoves :: Int -> Grid -> Int -> MinPrioHeap Int Grid
-getCorridorMoves roomSize grid@Grid{..} cost = Heap.unions $ Vector.toList $ Vector.imap newCorridor corridor
+getHeuristic :: Grid -> Int
+getHeuristic Grid{..} = roomHeuristic + corridorHeuristic
     where
-        newCorridor :: Int -> Maybe Amphipod -> MinPrioHeap Int Grid
+        corridorHeuristic = getCorridorHeuristic corridor
+        roomHeuristic = sum $ zipWith (getRoomHeuristic roomSize) [A,B,C,D] [roomA, roomB, roomC, roomD]
+
+getCorridorHeuristic :: [Maybe Amphipod] -> Int
+getCorridorHeuristic = sum . imap toMove
+    where
+        toMove :: Int -> Maybe Amphipod -> Int
+        toMove _ Nothing     = 0
+        toMove curr (Just a) = amphipodMoveCost a * abs (curr - getDest a)
+
+getRoomHeuristic :: Int -> Amphipod -> [Amphipod] -> Int
+getRoomHeuristic maxRoomSize a1 room = removeCost + addCost
+    where
+        roomSize = length room
+        correctRoom = replicate roomSize a1
+        Just toRemove = find (\x -> drop x room == drop x correctRoom) [0..roomSize]
+        removeCost = sum $ zipWith moveDist [1..] $ take toRemove room
+
+        toAdd = maxRoomSize - roomSize + toRemove
+        addCost = amphipodMoveCost a1 * (toAdd * (toAdd + 1)) `div` 2
+        roomLoc = getDest a1
+
+        moveDist :: Int -> Amphipod -> Int
+        moveDist roomMoves a2 = amphipodMoveCost a2 * (roomMoves + corridorMoves)
+            where corridorMoves = if a1 == a2 then 2 else abs (roomLoc - getDest a2)
+
+getCorridorMoves :: Grid -> Int -> MinPrioHeap AStarItem Grid
+getCorridorMoves grid@Grid{..} cost = Heap.unions $ imap newCorridor corridor
+    where
+        newCorridor :: Int -> Maybe Amphipod -> MinPrioHeap AStarItem Grid
         newCorridor _ Nothing = Heap.empty
         newCorridor i (Just a)
             | not canMove = Heap.empty
-            | otherwise = Heap.singleton (newCost, updatedRoom{corridor=corridor Vector.// [(i, Nothing)]})
+            | otherwise = Heap.singleton (AStarItem {gValue=newCost, hValue=getHeuristic finalGrid}, finalGrid)
             where
+                finalGrid = updatedRoom{corridor=setAt i Nothing corridor}
+
                 dest = getDest a
                 canMove = clearPath i dest corridor && checkRoom grid a
 
@@ -110,14 +153,16 @@ getCorridorMoves roomSize grid@Grid{..} cost = Heap.unions $ Vector.toList $ Vec
                     C -> (grid{roomC=C:roomC}, movesInRoom roomSize roomC)
                     D -> (grid{roomD=D:roomD}, movesInRoom roomSize roomD)
 
-getRoomMoves :: Int -> Grid -> Int -> Amphipod -> MinPrioHeap Int Grid
-getRoomMoves roomSize grid@Grid{..} cost a
+getRoomMoves :: Grid -> Int -> Amphipod -> MinPrioHeap AStarItem Grid
+getRoomMoves grid@Grid{..} cost a
     | checkRoom grid a = Heap.empty
     | otherwise = Heap.fromList $ map getNewGrids $ getReachable start
     where
-        getNewGrids :: Int -> (Int, Grid)
-        getNewGrids i = (cost + amphipodMoveCost amphi * (abs (start - i) + movesInRoom roomSize room + 1), newGrid{corridor=corridor Vector.// [(i, Just amphi)]})
-            where amphi = head room
+        getNewGrids :: Int -> (AStarItem, Grid)
+        getNewGrids i = (AStarItem {gValue = cost + amphipodMoveCost amphi * (abs (start - i) + movesInRoom roomSize room + 1), hValue = getHeuristic finalGrid}, finalGrid)
+            where
+                finalGrid = newGrid{corridor=setAt i (Just amphi) corridor}
+                amphi = head room
 
         (start, room, newGrid) = case a of
             A -> (2, roomA, grid{roomA=tail roomA})
@@ -126,23 +171,7 @@ getRoomMoves roomSize grid@Grid{..} cost a
             D -> (8, roomD, grid{roomD=tail roomD})
 
         getReachable :: Int -> [Int]
-        getReachable start = {- getReachableLeft start ++ getReachableRight start -} filter (\i -> i `notElem` invalidSpaces && clearPath start i corridor) [0..10]
-
-        getReachableLeft :: Int -> [Int]
-        getReachableLeft i
-            | i < 0 = []
-            | otherwise = case corridor Vector.! i of
-                Just _  -> []
-                Nothing -> if i `elem` invalidSpaces then next else i:next
-            where next = getReachableLeft (i-1)
-
-        getReachableRight :: Int -> [Int]
-        getReachableRight i
-            | i > 10 = []
-            | otherwise = case corridor Vector.! i of
-                Just _  -> []
-                Nothing -> if i `elem` invalidSpaces then next else i:next
-            where next = getReachableRight (i+1)
+        getReachable start = filter (\i -> i `notElem` invalidSpaces && clearPath start i corridor) [0..10]
 
 movesInRoom :: Int -> [Amphipod] -> Int
 movesInRoom roomSize = (roomSize -) . length
@@ -159,20 +188,21 @@ getDest B = 4
 getDest C = 6
 getDest D = 8
 
-clearPath :: Int -> Int -> Vector (Maybe Amphipod) -> Bool
+clearPath :: Int -> Int -> [Maybe Amphipod] -> Bool
 clearPath curr dest corridor
     | curr == dest = True -- should never happen
-    | otherwise = all isNothing $ Vector.slice sliceStart sliceSize corridor
+    | otherwise = all isNothing $ take sliceSize $ drop sliceStart corridor
     where
         (sliceStart, sliceSize) = if curr < dest
-                                  then (curr + 1, dest - curr - 1)
-                                  else (dest, curr - dest - 1)
+                                  then (curr + 1, dest - curr)
+                                  else (dest, curr - dest)
 
 part2 :: Input -> Output2
-part2 = search 4 (getTarget 4) Set.empty . Heap.singleton . (0,) . expandGrid
+part2 g = search Set.empty $ Heap.singleton (AStarItem 0 $ getHeuristic newGrid, newGrid)
+    where newGrid = expandGrid g
 
 expandGrid :: Grid -> Grid
-expandGrid Grid{..} = Grid{corridor=corridor, roomA=insertAt 1 [D, D] roomA, roomB=insertAt 1 [C, B] roomB, roomC=insertAt 1 [B, A] roomC, roomD=insertAt 1 [A, C] roomD}
+expandGrid Grid{..} = Grid{roomSize = 4, corridor=corridor, roomA=insertAt 1 [D, D] roomA, roomB=insertAt 1 [C, B] roomB, roomC=insertAt 1 [B, A] roomC, roomD=insertAt 1 [A, C] roomD}
 
 insertAt :: Int -> [a] -> [a] -> [a]
 insertAt n xs ys = take n ys ++ xs ++ drop n ys
